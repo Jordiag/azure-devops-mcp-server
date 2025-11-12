0e62b401b4b5a7bfa63ecb98107cdbdae766635b
Jordi Arquimbau <github@mozcode.com>
2025-11-12 23:46:13 +0000

    pull request property type update and nuget versions upgrade

--- src/Dotnet.AzureDevOps.Core/Dotnet.AzureDevOps.Core.Repos/Options/PullRequestCreateOptions.cs
+++ src/Dotnet.AzureDevOps.Core/Dotnet.AzureDevOps.Core.Repos/Options/PullRequestCreateOptions.cs
@@ -37,5 +37,5 @@ public record PullRequestCreateOptions
     public bool IsDraft { get; init; } = false;
 
     // You could add a list of reviewers, labels, etc. here if needed.
-    public string[]? Reviewers { get; init; }
+    public IEnumerable<string>? Reviewers { get; init; }
 }
